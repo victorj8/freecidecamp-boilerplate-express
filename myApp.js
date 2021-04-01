54725637e4dccfa3f780a38256a1421671367e2f
Victor Camacho <victor.camacho@block.one>
2021-04-01 09:02:07 -0400

    serve json

--- myApp.js
+++ myApp.js
@@ -2,6 +2,14 @@ var express = require('express');
 var app = express();
 app.use('/public', express.static(__dirname + '/public'));
 
+
+app.get(
+    '/json',
+    function(req, res){
+        res.json({"message": "Hello json"});
+    }
+);
+
 app.get(
     '/',
     function(req, res){
