0d66c592f27f68318a6552f06124800ec41a8aa6
Victor Camacho <victor.camacho@block.one>
2021-04-01 09:36:32 -0400

    added environment variable reading

--- myApp.js
+++ myApp.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var app = express();
 app.use('/public', express.static(__dirname + '/public'));
+const dotenv = require('dotenv');
+dotenv.config();
 
 
 app.get(
     '/json',
     function(req, res){
-        res.json({"message": "Hello json"});
+        var response_message = 'Hello json';
+        if(process.env.MESSAGE_STYLE == "uppercase") {
+            response_message = response_message.toUpperCase();
+        }
+
+        res.json({"message": response_message});
     }
 );
 
